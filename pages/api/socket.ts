13dcf548f9c1f37f238e5cc2cb814175e5b22a47
Pritam das (Ernst and Young LLP) <v-pritdas@microsoft.com>
2025-05-07 18:27:13 +0530

    Socket fixes

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,11 +1,20 @@
-import { NextApiRequest } from 'next';
-import { Server } from 'socket.io';
-import { NextApiResponseServerIO } from '../../types';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Server as IOServer } from 'socket.io';
+import { Server as HTTPServer } from 'http';
 
-export default function handler(req: NextApiRequest, res: NextApiResponseServerIO) {
-  if (!res.socket.server.io) {
-    const io = new Server(res.socket.server);
-    res.socket.server.io = io;
+// Initialize Socket.io on the Next.js server
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Next.js stores the Node HTTP server on res.socket.server
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const httpServer = (res.socket as any).server as HTTPServer;
+
+  // Create new Socket.io server if none exists
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (!(httpServer as any).io) {
+    const io = new IOServer(httpServer);
+    // Attach to the server so we don't recreate on every call
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (httpServer as any).io = io;
   }
   res.end();
 }
